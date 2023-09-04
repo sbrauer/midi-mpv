a30263f844491e82a4323b8948589d4433b68ec5
Sam Brauer <sam.brauer@gmail.com>
2023-09-04 18:11:36 -0400

    Add comments to sample mpv startup script

--- scripts/mpv-for-vj.sh
+++ scripts/mpv-for-vj.sh
@@ -1,1 +1,6 @@
-/Applications/mpv.app/Contents/MacOS/mpv --input-ipc-server=/tmp/mpv-socket.0 --keep-open=yes --keep-open-pause=no --loop-playlist --fullscreen --loop-file=inf --video-osd=no $*
+# Just an example; tweak to taste.
+# The most critical detail is that you give each MPV instance its own dedicated socket file.
+# The sample config.clj assumes a naming convention like `/tmp/mpv-socket.0`, `/tmp/mpv-socket.1`, etc.
+# Pass a dir or playlist argument to this script.
+MPV=/Applications/mpv.app/Contents/MacOS/mpv
+$MPV --input-ipc-server=/tmp/mpv-socket.0 --keep-open=yes --keep-open-pause=no --loop-playlist --fullscreen --loop-file=inf --video-osd=no $*

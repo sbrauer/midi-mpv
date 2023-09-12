0c7307659d97dc6970e82c45d6d8945b71b0be7e
Sam Brauer <sam.brauer@gmail.com>
2023-09-12 16:58:01 -0400

    Minor tweaks

--- mpv/scripts/sammy.lua
+++ mpv/scripts/sammy.lua
@@ -1,14 +1,8 @@
---[[
---
---Sam Brauer mpv stuff
---invoke with command like:
---```
---script-message-to sammy safe-playlist-play-index 23
---```
---
-]]--
+-- Sam Brauer's mpv stuff
+-- invoke with command like:
+-- script-message-to sammy safe-playlist-play-index 23
 
---like playlist-play-index command enhanced with bounds check
+-- like playlist-play-index command enhanced with bounds check
 mp.register_script_message("safe-playlist-play-index", function (idx)
     local count = mp.get_property_number("playlist-count")
     if tonumber(idx) < count then

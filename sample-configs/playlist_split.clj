0900cfc1861a61e0704abf7e2748522a382c5120
Sam Brauer <sam.brauer@gmail.com>
2023-09-12 17:18:23 -0400

    Reorganize sample configs

---
+++ sample-configs/playlist_split.clj
@@ -0,0 +1,50 @@
+;; Sample config file with keyboard split
+;; where left side triggers clips for first MPV
+;; and right side triggers clips for second MPV.
+;; Config is the map at the end of the file.
+
+(use 'midi-mpv.util)
+
+(def midi-device-name "IAC Driver IAC Bus 1")
+(def base-socket-path "/tmp/mpv-socket")
+
+;; Note values for keyboard split
+;; These match my 49 key midi keyboard (with default octave shift).
+;; Tweak to match your keyboard...
+(def start1 (octave 3))
+(def start2 (octave 5))
+(def end    (octave 7))
+
+;; Note this depend on a custom MPV script (included in the repo).
+;; The standard MPV playlist-play-index command would work
+;; if it tolerated index out of bounds, but instead MPV crashes!
+(defn playlist-command
+  [idx]
+  (str "script-message-to sammy safe-playlist-play-index " idx))
+
+(defn note->idx
+  "Return 0 when note is in first split
+  1 when note is in second split
+  or nil when note is out of range."
+  [note]
+  (when (>= note start1)
+    (if (< note start2)
+      0
+      (when (<= note end)
+        1))))
+
+(defn socket-path
+  [idx]
+  (format "%s.%s" base-socket-path idx))
+
+(defn midi-event->action
+  [{:keys [command note] :as event}]
+  (when (= :note-on command)
+    (when-let [idx (note->idx note)]
+      {:socket-path (socket-path idx)
+       :command (let [start (if (zero? idx) start1 start2)]
+                  (playlist-command (- note start)))})))
+
+;; This is it - the config map in all its glory...
+{:midi-device-name   midi-device-name
+ :midi-event->action midi-event->action}
---
+++ sample-configs/seek.clj
@@ -0,0 +1,61 @@
+;; Sample config file
+;; Config is the map at the end of the file.
+
+(use 'midi-mpv.util)
+
+(def midi-device-name "IAC Driver IAC Bus 1")
+(def base-socket-path "/tmp/mpv-socket")
+
+;; Prefs for pitch-bend -> speed
+(def top-speed 4.0)
+(def bottom-speed 0.25)
+
+(defn seek-commands
+  [start num]
+  (into {}
+        (for [n (range num)]
+          [(+ n start)
+           (if (zero? n)
+             "seek 0 absolute"
+             (format "seek %s absolute-percent" (* 100 (/ (float n) num))))])))
+
+(def note-commands
+  ;; These start at the low C of my 49 key midi keyboard (with default octave shift).
+  (seek-commands (octave 3) (inc (octave 4))))
+
+(defn handle-cc
+  [cc v]
+  (when (= 1 cc) ;; mod wheel
+    (let [percent (* 100 (/ v 127.0))]
+      (format "seek %s absolute-percent" percent))))
+
+(defn handle-pb
+  [v1 v2]
+  (let [pb (interpret-pitch-bend-data v1 v2)
+        normal-speed 1.0
+        speed (cond
+                (pos? pb) (+ normal-speed (* pb (- top-speed    normal-speed)))
+                (neg? pb) (+ normal-speed (* pb (- normal-speed bottom-speed)))
+                :default  normal-speed)]
+    (format "set speed %s" speed)))
+
+(defn event->command
+  [{:keys [command data1 data2] :as event}]
+  (case command
+    :note-on (get note-commands data1)
+    :control-change (handle-cc data1 data2)
+    :pitch-bend (handle-pb data1 data2)))
+
+(defn event->socket-path
+  [{:keys [channel] :as event}]
+  (format "%s.%s" base-socket-path channel))
+
+(defn midi-event->action
+  [event]
+  (when-let [command (event->command event)]
+    {:socket-path (event->socket-path event)
+     :command     command}))
+
+;; This is it - the config map in all its glory...
+{:midi-device-name   midi-device-name
+ :midi-event->action midi-event->action}
